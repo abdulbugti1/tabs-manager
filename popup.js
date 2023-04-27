d0de9eb98262e72a7d71d3e5274e71da1385b762
Abdul Nawaz Bugti <abdul@squirro.com>
2023-04-27 13:52:37 +0200

    Added error handling

--- popup.js
+++ popup.js
@@ -1,17 +1,21 @@
-async function getTabs(domainPattern, groupName) {
-  const tabs = await chrome.tabs.query({
-    url: [domainPattern],
-  });
-  const collator = new Intl.Collator();
-  tabs.sort((a, b) => collator.compare(a.title, b.title));
-  const tabIds = tabs.map(({ id }) => id);
-  const group = await chrome.tabs.group({ tabIds });
-  chrome.tabGroups.update(group, { title: groupName });
+async function groupTabs(domainPattern, groupName) {
+  try {
+    const tabs = await chrome.tabs.query({
+      url: [domainPattern],
+    });
+    const collator = new Intl.Collator();
+    tabs.sort((a, b) => collator.compare(a.title, b.title));
+    const tabIds = tabs.map(({ id }) => id);
+    const group = await chrome.tabs.group({ tabIds });
+    chrome.tabGroups.update(group, { title: groupName });
+  } catch (error) {
+    document.querySelector("#error").textContent = error;
+  }
 }
 
 const button = document.querySelector("button");
 button.addEventListener("click", async () => {
   let domainPattern = document.querySelector("#domain-pattern").value;
   let groupName = document.querySelector("#group").value;
-  getTabs(domainPattern, groupName);
+  groupTabs(domainPattern, groupName);
 });
