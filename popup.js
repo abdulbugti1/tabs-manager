f862c22a1af9e61e2f9d1b2c7e15987b659d5fe3
Abdul Nawaz Bugti <abdul@squirro.com>
2023-04-30 17:24:21 +0200

    Added option to update groups

--- popup.js
+++ popup.js
@@ -1,4 +1,4 @@
-async function groupTabs(domainPattern, groupName) {
+async function groupTabs(domainPattern, groupName, gColor = undefined) {
   try {
     const tabs = await chrome.tabs.query({
       url: [domainPattern],
@@ -7,15 +7,38 @@ async function groupTabs(domainPattern, groupName) {
     tabs.sort((a, b) => collator.compare(a.title, b.title));
     const tabIds = tabs.map(({ id }) => id);
     const group = await chrome.tabs.group({ tabIds });
-    chrome.tabGroups.update(group, { title: groupName });
+    let updateParams = {
+      title: groupName,
+      collapsed: true,
+      color: gColor,
+    };
+    chrome.tabGroups.update(group, updateParams);
+    localStorage[groupName] = domainPattern;
   } catch (error) {
     document.querySelector("#error").textContent = error;
   }
 }
 
-const button = document.querySelector("button");
-button.addEventListener("click", async () => {
+const groupTabsButton = document.querySelector("#group-tabs");
+const updateGroupsButton = document.querySelector("#update-groups");
+
+// Group Tabs
+groupTabsButton.addEventListener("click", async () => {
   let domainPattern = document.querySelector("#domain-pattern").value;
   let groupName = document.querySelector("#group").value;
   groupTabs(domainPattern, groupName);
+});
+
+// Update Groups
+updateGroupsButton.addEventListener("click", async () => {
+  chrome.tabGroups.query({}, function (groups) {
+    groups.forEach(function (group) {
+      // A group has following keys: collapsed,color,id,title,windowId
+      let gID = group.id;
+      let gName = group.title;
+      let gColor = group.color;
+      let gPattern = localStorage[gName];
+      groupTabs(gPattern, gName, gColor);
+    });
+  });
 });
